c695e1bccb2f802c816f120d5d5a9d4650bf7c1f
Asef Guluzade <asefdevs@gmail.com>
2024-02-05 20:59:01 +0300

    Add TOTP class for generating and verifying TOTP codes

---
+++ Authenctication/TOTP.cs
@@ -0,0 +1,30 @@
+using OtpNet;
+using System;
+
+namespace newProject.Services
+{
+    public class TOTP
+    {
+        public byte[] GenerateRandomKey()
+        {
+            var key = KeyGeneration.GenerateRandomKey(20);
+
+            var base32String = Base32Encoding.ToString(key);
+            var base32Bytes = Base32Encoding.ToBytes(base32String);
+            return base32Bytes;
+        }
+
+        public string GenerateTotpCode(byte[] secretKey)
+        {
+            var totp = new OtpNet.Totp(secretKey, step: 15, totpSize: 6);
+            var totpCode = totp.ComputeTotp(DateTime.UtcNow);
+            return totpCode;
+        }
+
+        public bool VerifyTotpCode(string totpCode, byte[] secretKey)
+        {
+            var totp = new OtpNet.Totp(secretKey, step: 15, totpSize: 6);
+            return totp.VerifyTotp(totpCode, out long timeStepMatched, new VerificationWindow(2, 2));
+        }
+    }
+}
